555dcdb6ca84fd318630b08d1925785092fb1fc0
ddukbaek2 <dagraac@naver.com>
2024-02-02 06:22:26 +0900

    work

--- ZM/Source/ZM.cs
+++ ZM/Source/ZM.cs
@@ -1,12 +1,50 @@
-﻿namespace ZM
+﻿using System.Collections.Generic;
+
+
+namespace ZM
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public class ZM<T>
-    {
-        public ZM()
-        {
-        }
-    }
+	//public interface IZM
+	//{
+	//	int Count { get; }
+
+	//	void Add(IZM zm);
+	//	void Remove(IZM zm);
+	//}
+
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ZM
+	{
+		private ZM m_Parent;
+		private List<ZM> m_Children;
+
+		public int Count
+		{
+			get
+			{
+				return m_Children.Count;
+			}
+		}
+
+		public ZM()
+		{
+			m_Parent = null;
+			m_Children.Clear();
+		}
+
+		public void SetParent(ZM model)
+		{
+			m_Parent = model;
+		}
+
+		public void Add(ZM model)
+		{
+		}
+
+		public void Remove(ZM model)
+		{
+		}
+	}
 }
